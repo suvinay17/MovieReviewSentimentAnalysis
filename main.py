efa9d854043a3d5574f11cf7d4062c5818f4b328
Yanish Rambocus <yanr@Yanishs-MacBook-Pro.local>
2020-04-22 16:22:54 +0400

    changing a few things around

--- main.py
+++ main.py
@@ -10,47 +10,72 @@ import glob
 import os
 import re
 
-#This code is for reading the multiple files in one folder into a list
-# read neg files in train folder
+# This code reads files in a folder into a list
+
+# read negative reviews train folder
 neg_files = glob.glob(os.path.join(os.getcwd(), "data/train/neg", "*.txt"))
 neg_list = []
 for neg_path in neg_files:
     with open(neg_path) as neg_input:
         neg_list.append(neg_input.read())
-#test purpose
-#print(neg_list[1])
 
-# read pos files in train folder
+# read positive reviews in train folder
 pos_files = glob.glob(os.path.join(os.getcwd(), "data/train/pos", "*.txt"))
 pos_list = []
 for pos_path in pos_files:
     with open(pos_path) as pos_input:
         pos_list.append(pos_input.read())
-#test purpose
-#print(len(pos_list))
+#For testing
+
+"""
+print(neg_list[1])
+print(len(pos_list))
+"""
+
 number_of_reviews = len(pos_list)+len(neg_list)
 
+# Method to clean reviews.
+# Gets rid of html tags inherited from imdb databases punctuation marks
+def preprocess_reviews(reviews):
 
-def extract_dictionary():
+    REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
+    REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+
+    reviews = [REPLACE_NO_SPACE.sub("", line.lower()) for line in reviews]
+    reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
+
+    return reviews
+
+preprocess_reviews(pos_list)
+preprocess_reviews(neg_list)
+
+def extract_dictionary(reviews):
     """
     Reads list of distinct words
     mapping from each distinct word to its index .
     Returns:
         a dictionary of distinct words that maps each distinct word
         to a unique index corresponding to when it was first found
-
     """
-    print(neg_list[0])
-    index = 0
     ind = 0
     word_dict = {}
+
+    for(i in len(reviews)):
+      splits = reviews[i].split()
+      for split in splits:
+        word_dict[split] = ind
+        ind += 1
+
+    return word_dict
+
     while(index <  1): # len(pos_list)):
-        regex = re.compile(r'[^a-zA-Z ]+')
+        regex = re.compile(r'<>[^a-zA-Z ]+')
         pos_list[index] = re.sub(regex, '', pos_list[index]).lower()
         # split string
         splits = pos_list[index].split()
         index = index + 1
-    # for loop to iterate over words array
+
+        # for loop to iterate over words array
         for split in splits:
             word_dict[split] = ind
             ind = ind + 1
